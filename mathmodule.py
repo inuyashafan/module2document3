9da7e5d17e9da9eecfd6039e6869e4b36ee5121a
John Abreu <johnabreu67@gmail.com>
2023-07-27 09:06:40 -0500

    first commit

---
+++ mathmodule.py
@@ -0,0 +1,13 @@
+def area_finder(L,W):
+    """find the area of a house"""
+    area = L*W
+    print(area)
+
+area_finder(5, 10)
+
+def circum_finder(r):
+    C = 2 * 3.14 * r
+    print(C)
+    
+
+circum_finder(3)
